c507a4313c5d525525682cbadce388f2716a9158
KaixoCode <jeroenzwiers2@gmail.com>
2021-12-05 00:41:13 +0100

    Update EntryPoint.cpp

--- source/EntryPoint.cpp
+++ source/EntryPoint.cpp
@@ -8,41 +8,13 @@ struct MySynth : public Synth
         using Voice<MySynth>::Voice;
 
         Oscillator& osc = Add<Oscillator>();
+        Oscillator& lfo = Add<Oscillator>({ .frequency = 0.5, .wavetable = Wavetables::sine });
+        ADSR& gain      = Add<ADSR>({ .release = 2 });
+        ADSR& filter    = Add<ADSR>({ .attack = 0.5, .decay = 5.5, .sustain = 0, .release = 2, .attackCurve = 0.9, .decayCurve = 0.2, .legato = true });
+        Chorus& chorus  = Add<Chorus>({ .oscillator{ { .frequency = 3, .wavetable = Wavetables::sine } } });
+        LPF& lowpass    = Add<LPF>({ .resonance = 1 });
 
-        Oscillator& lfo = Add<Oscillator>({ 
-            .frequency = 0.5, 
-            .wavetable = Wavetables::sine 
-        });
-
-        ADSR& gain = Add<ADSR>({
-            .release = 2,    
-        });
-
-        ADSR& filter = Add<ADSR>({
-            .attack = 0.5,
-            .decay = 5.5, 
-            .sustain = 0, 
-            .release = 2,
-            .attackCurve = 0.9,
-            .decayCurve = 0.2,
-            .legato = true
-        });
-
-        Chorus& chorus = Add<Chorus>({ 
-            .oscillator{ {
-                .frequency = 3, 
-                .wavetable = Wavetables::sine 
-            } } 
-        });
-
-        LPF& lowpass = Add<LPF>({
-            .resonance = 1 
-        });
-
-        ChainFun Chain() override 
-        {
-            return osc >> gain >> lowpass >> chorus;
-        }
+        ChainFun Chain() override { return osc >> gain >> lowpass >> chorus; }
 
         void Mod() override
         {
@@ -57,48 +29,18 @@ struct MySynth : public Synth
         void NotePress(int n, int velocity) override
         {
             osc.settings.frequency = noteToFreq(n);
-            gain.Gate(true);
-            filter.Gate(true);
+            gain.Gate(true), filter.Gate(true);
         }
 
         void NoteRelease(int n) override { gain.Gate(false); filter.Gate(false); }
         bool Done() override { return gain.Done() && filter.Done(); }
     };
 
-    Parameter& chorusMix = emplace_back<Parameter>({
-        .value = 50,
-        .range = { 0, 100 },
-        .name = "Chorus",
-        .unit = Units::PERCENT,
-    });
-
-    Parameter& delayMix = emplace_back<Parameter>({
-        .value = 50,
-        .range = { 0, 100 },
-        .name = "Delay",
-        .unit = Units::PERCENT,
-    });
-    
-    Parameter& filterMix = emplace_back<Parameter>({
-        .value = 100,
-        .range = { 0, 100 },
-        .name = "Filter",
-        .unit = Units::PERCENT,
-    });
-
-    Parameter& filterReso = emplace_back<Parameter>({
-        .value = 0.6,
-        .range = { 0.2, 6 },
-        .name = "Res",
-        .unit = Units::NONE,
-    });
-
-    Parameter& gainP = emplace_back<Parameter>({
-        .value = 0,
-        .range = { -24, 24 },
-        .name = "Gain",
-        .unit = Units::DECIBEL,
-    });
+    Parameter& chorusMix  = emplace_back<Parameter>({ .value = 50,  .range{ 0, 100 },  .name = "Chorus", .unit = Units::PERCENT });
+    Parameter& delayMix   = emplace_back<Parameter>({ .value = 50,  .range{ 0, 100 },  .name = "Delay",  .unit = Units::PERCENT });
+    Parameter& filterMix  = emplace_back<Parameter>({ .value = 100, .range{ 0, 100 },  .name = "Filter", .unit = Units::PERCENT });
+    Parameter& filterReso = emplace_back<Parameter>({ .value = 0.6, .range{ 0.2, 6 },  .name = "Res",    .unit = Units::NONE    });
+    Parameter& gainP      = emplace_back<Parameter>({ .value = 0,   .range{ -24, 24 }, .name = "Gain",   .unit = Units::DECIBEL });
 
     Delay& delay = Add<Delay>();
     Gain& gain = Add<Gain>();
